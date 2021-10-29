5b8f64312d82c8886ce1c0959722e12c5373ba8c
bhomik33 <69043188+bhomik33@users.noreply.github.com>
2021-10-29 17:42:21 +1100

    Add files via upload

---
+++ CheckInternetSpeed/index.js
@@ -0,0 +1,39 @@
+import fetch from "node-fetch";
+import {AbortController} from "node-abort-controller";
+import {performance} from 'perf_hooks';
+
+async function checkInternetSpeed(URI) {
+    // controller is used to abort a fetch request
+    const controller = new AbortController();
+
+    // abort function runs after 5 secs
+    setTimeout(() => controller.abort(), 5000);
+    const startTime = performance.now();
+    try {
+        // making a fetch request to the URI
+        // connecting controller to the fetch request
+        await fetch(URI, {
+            "signal": controller.signal
+        });
+        const finishTime = performance.now();
+
+        // calculating response time and converting milliseconds into seconds
+        const responseTime = (finishTime - startTime) / 1000;
+
+        // returning good if response time is less than half a second
+        if (responseTime < 0.5) {
+            return 'good';
+        }
+
+        // the else block would run if the response time is more thant half second but less than 5 seconds
+        else {
+            return 'fine';
+        }
+    }
+    // the catch block would be executed if fetch request gets aborted or takes more than 5 sec
+    catch {
+        return 'terrible';
+    }
+}
+
+console.log(await checkInternetSpeed('https://deelay.me/5000/https://picsum.photos/200/300'));
